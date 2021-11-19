8d913f146d9f6fa1da62f8a92fdb94bcd5e4a8fc
CibleArtist <1934013aiml@cit.edu.in>
2021-11-20 03:52:20 +0530

    Pages

--- profile.js
+++ profile.js
@@ -1,15 +1,33 @@
-function saveEdits() {
+var text = document.getElementById('myText');
+var myData;
+var postData = window.localStorage.getItem("save");
+var reset = text.innerHTML;
+// if no data
+if (postData == null || postData == '') {
+    myData = text.innerHTML;
+    // store default value
+    window.localStorage.setItem("save", myData);
+    // make it placeholder style
+    text.classList.remove('changed');
+} else {
+    // if there is a value post it
+    text.innerHTML = postData;
+    // make dark text
+    text.classList.add('changed');
+}
 
-    //get the editable element
-    var editElem = document.getElementById("Edit");
-    
-    //get the edited element content
-    var userVersion = editElem.innerHTML;
-    
-    //save the content to local storage
-    localStorage.userEdits = userVersion;
-    
-    //write a confirmation to the user
-    document.getElementById("update").innerHTML="Edits saved!";
-    
+function saveEdits() {
+    // store the current value
+    myData = text.innerHTML;
+    // local store the value
+    window.localStorage.setItem("save", myData);
+    text.classList.add('changed');
+}
+
+function clearStorage() {
+    text.classList.remove('changed');
+    // clear storage
+    window.localStorage.clear("save");
+    // return to default text
+    text.innerHTML = reset;
 }
